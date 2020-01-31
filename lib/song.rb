f67410b695b2eac2d07c257dff9848b451f5573b
Michael Waldo <mbwcogic@yahoo.com>
2020-01-31 00:44:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,6 +5,7 @@ class Song
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   include Paramable::InstanceMethods
+  extend Memorable::ClassMethods
   attr_accessor :name
   attr_reader :artist
 
@@ -12,10 +13,6 @@ class Song
 
   def initialize
     self.class.all << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
   end
 
   def self.all
