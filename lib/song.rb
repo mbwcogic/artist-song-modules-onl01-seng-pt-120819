b8aa654b56b66bee7564ec8eb54c7b57a7824e18
Michael Waldo <mbwcogic@yahoo.com>
2020-01-31 00:44:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   include Paramable::InstanceMethods
-  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
   attr_accessor :name
   attr_reader :artist
 
